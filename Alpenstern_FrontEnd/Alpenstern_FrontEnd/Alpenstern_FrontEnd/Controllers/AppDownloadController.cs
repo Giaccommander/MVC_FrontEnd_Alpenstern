d598dc46c50ab2922d05bb32d4c8d86583c6b95a
cybermotte <cybermotte@vollbio.de>
2017-06-29 13:23:46 +0200

    ?!?

---
+++ Alpenstern_FrontEnd/Alpenstern_FrontEnd/Alpenstern_FrontEnd/Controllers/AppDownloadController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpenstern_FrontEnd.Controllers
+{
+    public class AppDownloadController : Controller
+    {
+        // GET: AppDownload
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
