fa00f9e9d59a9d8b525ba5d1d914a553efaabde3
s.wolfrath <s.wolfrath@gmx.at>
2017-06-29 12:16:47 +0200

    F-14-Zimmer

---
+++ Alpenstern_FrontEnd/Alpenstern_FrontEnd/Controllers/ZimmerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpenstern_FrontEnd.Controllers
+{
+    public class ZimmerController : Controller
+    {
+        // GET: Zimmer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
