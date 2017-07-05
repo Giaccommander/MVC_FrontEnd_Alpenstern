c589b0119d738f4762c6cde3ff1320248e50f880
StevenWoerz <woerz.steven@gmail.com>
2017-07-05 16:25:45 +0200

    Bilder eingefügt und formatiert

--- Alpenstern_FrontEnd/Alpenstern_FrontEnd/Controllers/ZimmerController.cs
+++ Alpenstern_FrontEnd/Alpenstern_FrontEnd/Controllers/ZimmerController.cs
@@ -17,5 +17,29 @@ namespace Alpenstern_FrontEnd.Controllers
         {
             return View();
         }
+        public ActionResult kEinzelzimmer()
+        {
+            return View();
+        }
+        public ActionResult Juniorsuite()
+        {
+            return View();
+        }
+        public ActionResult SuiteTirol()
+        {
+            return View();
+        }
+        public ActionResult Zirbensuite()
+        {
+            return View();
+        }
+        public ActionResult Gletschersuite()
+        {
+            return View();
+        }
+        public ActionResult SuiteAlpenstern()
+        {
+            return View();
+        }
     }
 }
