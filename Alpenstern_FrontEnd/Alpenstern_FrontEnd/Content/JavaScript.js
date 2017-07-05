a116c2ca74df5ac1e7b6ac4456b3607b5018bd51
austrianBraveheart <tom.140774@gmail.com>
2017-07-05 16:26:49 +0200

    hotelGalleryNeu

--- Alpenstern_FrontEnd/Alpenstern_FrontEnd/Content/JavaScript.js
+++ Alpenstern_FrontEnd/Alpenstern_FrontEnd/Content/JavaScript.js
@@ -41,3 +41,23 @@ $(document).ready(function () {
 
 
 // #endregion
+
+// #region Gallery
+
+$(document).ready(function () {
+    $("[class^='thumbnail-']").click(function () {
+        $("[class^='thumbnail-']").slideToggle("fast");
+        $(this).next("[class^='large-']").slideToggle();
+    });
+
+    $(".close").click(function () {
+        $("[class^='large-']:visible").toggle();
+        $("[class^='thumbnail-']").fadeToggle("fast");
+    });
+
+});
+
+new WOW().init();
+
+
+// #endregion
