77fc91a9d9d1b890308e16a1539f9dcab7f6650d
bintiM <marc.bintinger@yahoo.com>
2017-07-08 08:57:00 +0200

    repariert2

---
+++ Alpenstern_FrontEnd/Alpenstern_FrontEnd/Controllers/KulinarikController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpenstern_FrontEnd.Controllers
+{
+    public class KulinarikController : Controller
+    {
+        //test//
+        // GET: Kulinarik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
