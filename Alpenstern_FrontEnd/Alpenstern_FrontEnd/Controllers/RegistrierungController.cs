5d3e58a773167caa45616f8e065abc0ee697447b
markus.prethaler <markus.prethaler@PC27>
2017-09-08 10:28:35 +0200

    RegistrierungController erstellt

---
+++ Alpenstern_FrontEnd/Alpenstern_FrontEnd/Controllers/RegistrierungController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Alpenstern_FrontEnd.Models;
+using Alpenstern_FrontEnd.Helper;
+using System.Data.SqlClient;
+
+namespace Alpenstern_FrontEnd.Controllers
+{
+    public class RegistrierungController : Controller
+    {
+        // GET: Registrierung
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
